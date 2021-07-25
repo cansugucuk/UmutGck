264a763746d52f97723235b925149090c19131b4
UmutGck <gucukumut@gmail.com>
2021-07-25 17:44:23 +0300

    ysazarkategorivsayfaları  ekleme

---
+++ LibraryApplication/Models/AuthorViewModel.cs
@@ -0,0 +1,26 @@
+﻿using LibraryApplication.DbModel.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LibraryApplication.Models
+{
+    public class AuthorViewModel
+    {
+      
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(100)]
+        [Display(Name = "Ad")]
+        public string FirstName { get; set; }
+
+        [StringLength(100)]
+        [Display(Name = "Soyad")]
+        public string Lastname { get; set; }
+
+        public ICollection<Book> Books { get; set; }
+    }
+}
