7885925324ac070b8cdfd7b4030cce8e130a09bb
cansugucuk <64957738+cansugucuk@users.noreply.github.com>
2022-01-21 03:33:31 +0300

    öğretmen öğrenci ve admin giriş kontrol error

---
+++ LibraryApplication/Controllers/StudentLoginController.cs
@@ -0,0 +1,44 @@
+﻿using LibraryApplication.DbModel.Context;
+using LibraryApplication.DbModel.Entity;
+using LibraryApplication.Models.Login;
+using System.Linq;
+using System.Web.Mvc;
+
+
+namespace LibraryApplication.Controllers
+{
+    public class StudentLoginController : Controller
+    {
+        // GET: Login
+
+            //kodlarım 
+        public ActionResult Login()
+        {
+
+            return View("~/Views/Login/StudentLogin/StudentLogin.cshtml");
+        }
+
+
+        //kodlarım 
+        //[HttpGet]
+        //public ActionResult Create([Bind(Include = "UserName,Password")] UserType userType)
+        [HttpPost]
+        public ActionResult Login([Bind(Include = "Username,Password")]Admin model)
+        {
+            DataBaseContext dbContext = new DataBaseContext();
+
+            var user = dbContext.Students.FirstOrDefault(p=>p.Username == model.Username && p.Password == model.Password);
+            if (user == null)
+            {
+                return View("~/Views/Login/Error.cshtml");
+            }
+            else
+            {
+                Session["LoginUser"] = user;
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+
+    }
+}
---
+++ LibraryApplication/Controllers/TeacherLoginController.cs
@@ -0,0 +1,44 @@
+﻿using LibraryApplication.DbModel.Context;
+using LibraryApplication.DbModel.Entity;
+using LibraryApplication.Models.Login;
+using System.Linq;
+using System.Web.Mvc;
+
+
+namespace LibraryApplication.Controllers
+{
+    public class TeacherLoginController : Controller
+    {
+        // GET: Login
+
+            //kodlarım 
+        public ActionResult Login()
+        {
+
+            return View("~/Views/Login/TeacherLogin/TeacherLogin.cshtml");
+        }
+
+
+        //kodlarım 
+        //[HttpGet]
+        //public ActionResult Create([Bind(Include = "UserName,Password")] UserType userType)
+        [HttpPost]
+        public ActionResult Login([Bind(Include = "Username,Password")]Admin model)
+        {
+            DataBaseContext dbContext = new DataBaseContext();
+
+            var user = dbContext.Teachers.FirstOrDefault(p=>p.Username == model.Username && p.Password == model.Password);
+            if (user == null)
+            {
+                return View("~/Views/Login/Error.cshtml");
+            }
+            else
+            {
+                Session["LoginUser"] = user;
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+
+    }
+}
